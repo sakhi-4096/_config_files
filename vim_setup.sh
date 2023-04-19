d83a7219b521479376e4acb74e6d16c94c2ebd26
Sakhile <sakhilelindah@protonmail.com>
2023-04-20 01:39:04 +0200

    Update vim setup script

--- vim_setup.sh
+++ vim_setup.sh
@@ -2,7 +2,7 @@
 
 # Install vim and YouCompleteMe with clang completion
 mkdir -p ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/Vundle.vim
 vim -c 'PluginInstall' -c 'qa!'
 cd ~/.vim/bundle/YouCompleteMe/
 python3 install.py --clang-completer
